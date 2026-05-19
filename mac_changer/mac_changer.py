52f5c58094675d6397ddf199711fa74efc0461d0
Likhith Kumar B <likhith15kumar@gmail.com>
2026-05-19 13:25:53 +0530

    Refactored the code

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -3,6 +3,23 @@
 import argparse
 import subprocess
 
+def change_mac(interface,new_mac):
+    print(f"[+] The mac address of {interface} is changed to {new_mac}")
+
+    subprocess.run([
+        "sudo","ip","link","set","dev",interface,"down"
+        ],check=True)
+
+    subprocess.run([
+        "sudo","ip","link","set","dev",interface,"address",new_mac
+        ],check=True)
+
+    subprocess.run([
+        "sudo","ip","link","set","dev",interface,"up"
+        ],check=True)
+
+
+
 parser = argparse.ArgumentParser(
     description="Mac changer"
     )
@@ -13,21 +30,6 @@ parser.add_argument(
     "-m","--mac",required=True,help="New Mac address"
 )
 args = parser.parse_args()
-interface = args.interface
-new_mac = args.mac
-
-print(f"[+] The mac address of {interface} is changed to {new_mac}")
-
-subprocess.run([
-    "sudo","ip","link","set","dev",interface,"down"
-    ],check=True)
-
-subprocess.run([
-    "sudo","ip","link","set","dev",interface,"address",new_mac
-    ],check=True)
-
-subprocess.run([
-    "sudo","ip","link","set","dev",interface,"up"
-    ],check=True)
+change_mac(args.interface,args.mac)
 
 
