e64a6199b72e4b8f787286f39d11454cc9b90fa5
Likhith Kumar B <likhith15kumar@gmail.com>
2026-05-21 23:21:47 +0530

    Add client parsing and result display to ARP scanner

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,12 +1,20 @@
 import scapy.all as scapy
 
 def scan(ip):
-    arp_requeste = scapy.ARP(pdst=ip)
-    brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_requeste_brodcast = brodcast/arp_requeste
-    answered,unanswered = scapy.srp(arp_requeste_brodcast,timeout=1)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered,unanswered = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)
+    client_list = []
     for elements in answered:
-        print(elements[0].psrc)
-        print(elements[0].hwsrc)
+        client_dict = {"ip":elements[1].psrc,"mac":elements[1].hwsrc}
+        client_list.append(client_dict)
+    return client_list
+        
+def print_result(result):
+    print("IP address \t\t\t Mac Address"+"\n---------------------------------------------------------------")
+    for element in result:
+        print(element["ip"]+"\t\t"+element["mac"])
 
-scan("192.168.122.1/24")
+result = scan("192.168.122.1/24")
+print_result(result)
