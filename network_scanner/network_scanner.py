2d1319dbcef56e810d5cf5e74f692f584cd40a69
Likhith Kumar B <likhith15kumar@gmail.com>
2026-05-21 15:58:40 +0530

    created broadcast requeste and arp requeste

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,6 +1,8 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arpping(ip)
-
+    arp_requeste = scapy.ARP(pdst=ip)
+    brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_requeste_brodcast = brodcast/arp_requeste
+    arp_requeste_brodcast.show()
 scan("192.168.122.1/24")
