f41b6a24cde6dc0f3af54595876bf81dd0859c2d
Likhith Kumar B <likhith15kumar@gmail.com>
2026-05-21 16:51:06 +0530

    refactored the code

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -4,5 +4,9 @@ def scan(ip):
     arp_requeste = scapy.ARP(pdst=ip)
     brodcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_requeste_brodcast = brodcast/arp_requeste
-    arp_requeste_brodcast.show()
+    answered,unanswered = scapy.srp(arp_requeste_brodcast,timeout=1)
+    for elements in answered:
+        print(elements[0].psrc)
+        print(elements[0].hwsrc)
+
 scan("192.168.122.1/24")
