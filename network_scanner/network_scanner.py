a5c94028635582676b639f10d511be699b05dc64
Likhith Kumar B <likhith15kumar@gmail.com>
2026-05-25 15:00:10 +0530

    added user input

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,20 +1,27 @@
 import scapy.all as scapy
 
+
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered,unanswered = scapy.srp(arp_request_broadcast,timeout=1,verbose=False)
+    arp_request_broadcast = broadcast / arp_request
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
     client_list = []
     for elements in answered:
-        client_dict = {"ip":elements[1].psrc,"mac":elements[1].hwsrc}
+        client_dict = {"ip": elements[1].psrc, "mac": elements[1].hwsrc}
         client_list.append(client_dict)
     return client_list
-        
-def print_result(result):
-    print("IP address \t\t\t Mac Address"+"\n---------------------------------------------------------------")
-    for element in result:
-        print(element["ip"]+"\t\t"+element["mac"])
 
-result = scan("192.168.122.1/24")
+
+def print_result(result):
+    print(
+        "IP address \t\t\t Mac Address"
+        + "\n---------------------------------------------------------------"
+    )
+    for element in result:
+        print(element["ip"] + "\t\t" + element["mac"])
+
+
+ip = input("Enter IP range (example: 192.168.1.1/24): ")
+result = scan(ip)
 print_result(result)
